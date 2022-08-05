828990692810b2e2af62bceceb61a73a143b32e0
g-xiangyi <gooeymi@hotmail.com>
2022-08-05 02:23:26 -0400

    Create header.php, body.php, footer.php
    largely forms front-end of website. body.php contains database fetching. Tables need to be created in MySQL.

---
+++ footer.php
@@ -0,0 +1,28 @@
+<section class="cid-" id="footer1-2">
+
+    <div class="container">
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">Address:</h5>
+                <p class="mbr-text">1385 Woodroffe Ave, Ottawa, ON <br>K2G 1V8</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">Contact:</h5>
+                <p class="mbr-text">Email: kungfu@algonquinlive.com</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">Links</h5>
+                <p class="mbr-text">Opportunities&nbsp;<br>Career&nbsp;<br><a href="#top">Back to top</a></p>
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row mbr-white">
+                <div class="col-sm-6 copyright">
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        © Copyright 2022 Amy Guo, Tianxu Pan
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
